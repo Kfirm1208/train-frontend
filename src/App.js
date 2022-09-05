6ef71392dc03f44b9e9ee7d1a8ea4bb2910196e3
Kfirm1208 <106590971+Kfirm1208@users.noreply.github.com>
2022-09-05 20:17:52 +0700

    git committed

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
   return (
     <div className ='App'>
      Count : {count}
-     <button className="button" onClick={()=> setCount(prev => prev + 1)}>+</button>
-     <button className ="button" onClick={()=> setCount(prev => prev - 1)}>-</button>
+     <button className="button_red" onClick={()=> setCount(prev => prev + 1)}>+</button>
+     <button className ="button_green" onClick={()=> setCount(prev => prev - 1)}>-</button>
     </div>
   
   
